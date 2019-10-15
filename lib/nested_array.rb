5d498740289f38b1ee16a4834379b9bb1aa8d574
Will Drougas <wdrougas@gmail.com>
2019-10-15 19:25:02 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -21,7 +21,7 @@ def assembled_matrix
 end
 
 def array_literal_matrix
-  [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+  [ENNET_HOUSE.sort, ENFIELD_TENNIS_ACADEMY.sort]
 end
 
 def matrix_lookup(matrix, row, column)
