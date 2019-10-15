c491925dab3241d003cef0289930acc22245433c
Will Drougas <wdrougas@gmail.com>
2019-10-15 19:34:36 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,4 +31,6 @@ end
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
+  
+  assembled_matrix[]
 end
