884a6d7818046dc593086edd42659294fc9d8bfe
Will Drougas <wdrougas@gmail.com>
2019-10-15 19:29:06 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -25,7 +25,7 @@ def array_literal_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  assembled_matrix[1][0]
+  assembled_matrix[1][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
